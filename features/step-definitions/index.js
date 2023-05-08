2392372aecb0a47606686bae4076d68d699b7946
zearkiatos <caprilespe@outlook.com>
2023-05-08 10:17:10 -0400

    Step definitions

--- features/step-definitions/index.js
+++ features/step-definitions/index.js
@@ -1,1 +1,37 @@
-//Complete siguiendo las instrucciones del taller
+const { Given } = require("cucumber");
+const { When } = require("cucumber");
+const { Then } = require("cucumber");
+const { expect } = require("chai");
+
+Given("I go to losestudiantes home screen", () => {
+  browser.url("/uniandes/");
+  if ($("button=Cerrar").isDisplayed()) {
+    $("button=Cerrar").click();
+  }
+});
+
+When("I open the login screen", () => {
+  $("button.loginButton").waitForExist(5000);
+  $("button.loginButton").waitForDisplayed(5000);
+  $("button.loginButton").click();
+});
+
+When("I fill a wrong email and password", () => {
+  const cajaLogIn = $(".cajaLogIn");
+
+  const mailInput = $('input[name="email"]');
+  mailInput.click();
+  mailInput.setValue("wrongemail@example.com");
+
+  const passwordInput = $('input[name="password"]');
+  passwordInput.click();
+  passwordInput.setValue("123467891");
+});
+
+When("I try to login", () => {
+  $("button=Ingresar").click();
+});
+
+Then("I expect to not be able to login", () => {
+  $(".notice.alert.alert-danger").waitForDisplayed(5000);
+});
